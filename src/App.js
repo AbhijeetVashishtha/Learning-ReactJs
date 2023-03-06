635feb33361e746da859b9abfe5b6cb5a8252aff
AbhijeetVashishtha <abhijeetvashishth13>
2023-03-07 00:27:22 +0530

    Rendering Expenses Dynamically

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
+import {useState} from 'react';
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
-  let expenses = [
-    {
-      id: 'e1',
-      title: 'Insurance',
-      amount: 3000,
-      date: new Date(2020, 7,14),
-    },
-    {
-      id: 'e2',
-      title: 'Toilet Paper',
-      amount: 95,
-      date: new Date(2021, 8, 23),
-    },
-    {
-      id: 'e3',
-      title: 'Newspaper',
-      amount: 5,
-      date: new Date(2023, 1, 8),
-    },
-    {
-      id: 'e4',
-      title: 'Pen',
-      amount: 20,
-      date: new Date(2023, 2, 50),
-    }
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Insurance',
+    amount: 3000,
+    date: new Date(2020, 7,14),
+  },
+  {
+    id: 'e2',
+    title: 'Toilet Paper',
+    amount: 95,
+    date: new Date(2021, 8, 23),
+  },
+  {
+    id: 'e3',
+    title: 'Newspaper',
+    amount: 5,
+    date: new Date(2022, 1, 8),
+  },
+  {
+    id: 'e4',
+    title: 'Pen',
+    amount: 20,
+    date: new Date(2022, 2, 50),
+  }
 ]; 
 
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
-  }
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
+  };
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
